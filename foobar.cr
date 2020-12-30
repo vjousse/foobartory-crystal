822651ac896bc2ec0292b579a482d49604a33d45
Vincent Jousse <vincent@jousse.org>
2020-12-30 14:58:53 +0000

    Wait for fibers to finish with channels

--- foobar.cr
+++ foobar.cr
@@ -9,20 +9,33 @@ end
 class Robot
   @last_activity : Nil | Activity
   @id : Int32
+  @changing_activity_time : Int32
 
   def initialize(id : Int32)
     @id = id
+    @changing_activity_time = 5
   end
 
-  def choose_activity()
+  def choose_activity(channel)
     new_activity = Activity.new(Random.new.rand(Activity.values.size))
     if new_activity != @last_activity
-      puts "Changing activity to #{new_activity}"
+      puts "Changing activity from #{@last_activity} to #{new_activity}"
+
+      # Changing activities takes X seconds
+      sleep @changing_activity_time.seconds
       @last_activity = new_activity
+
+      puts "Activity changed to #{new_activity}"
+    else
+      puts "Activity is the same #{new_activity}"
     end
+
+    channel.send(nil)
   end
 
 end
+
+channel = Channel(Nil).new
 
 start_time = Time.monotonic
 
@@ -33,10 +46,20 @@ robots = [] of Robot
 robots_size.times do |i|
   robot = Robot.new i
   robots << robot
-  spawn robot.choose_activity
 end
 
-Fiber.yield
+# Do 6 rounds for now
+6.times do
+  # Spawn a task per robot
+  robots.each do |robot|
+    spawn robot.choose_activity(channel)
+  end
+
+  robots_size.times do |i|
+    channel.receive
+  end
+end
+
 
 puts robots
 
