96bf8716fb6f5645161c7c06dd01a0cb31a4b0f5
Vincent Jousse <vincent@jousse.org>
2020-12-30 17:56:46 +0000

    Construct some real FooBar

--- foobar.cr
+++ foobar.cr
@@ -8,7 +8,16 @@ enum Activity
   SellFooBar
 end
 
-alias FooBar = Tuple(Foo, Bar)
+class FooBar
+  def initialize(foo : Foo, bar : Bar)
+    @foo = foo
+    @bar = bar
+  end
+
+  def assemble
+    "#{@foo}/{@bar}"
+  end
+end
 
 class Foo
   def initialize(value : String)
@@ -16,32 +25,44 @@ class Foo
   end
 end
 
-
 class Bar
   def initialize(value : String)
     @value = value
   end
 end
 
-
 class Robot
   @last_activity : Nil | Activity
   @id : Int32
-  @changing_activity_time : Int32
+  @changing_activity_time : Float64
   @mine_bar_min_time : Float64
   @mine_bar_max_time : Float64
 
-  def initialize(id : Int32, channel : Channel(Nil|Foo|Bar|FooBar))
+  def initialize(id : Int32, channel : Channel(Nil | Foo | Bar | FooBar), stock_channel : Channel(Array(Foo) | Array(Bar)))
     @id = id
-    @changing_activity_time = 5
-    @mine_foo_time = 1
-    @mine_bar_min_time = 0.5
-    @mine_bar_max_time = 2
+    @changing_activity_time = 0
+    @mine_foo_time = 0
+    @mine_bar_min_time = 0
+    @mine_bar_max_time = 0
     @channel = channel
+    @stock_channel = stock_channel
   end
 
-  def choose_activity()
+  def choose_activity
     new_activity = Activity.new(Random.new.rand(Activity.values.size))
+    foos = [] of Foo
+    bars = [] of Bar
+
+    2.times do
+      value = @stock_channel.receive
+      case value
+      when Array(Foo)
+        foos = value
+      when Array(Bar)
+        bars = value
+      end
+    end
+
     if new_activity != @last_activity
       puts "#{@id} - Changing activity from #{@last_activity} to #{new_activity}"
 
@@ -62,15 +83,23 @@ class Robot
       bar = self.mine_bar
       @channel.send(bar)
     when Activity::AssembleFooAndBar
-      bar = self.mine_bar
-      @channel.send(bar)
+      latest_foo = foos.pop?
+      latest_bar = bars.pop?
+
+      if  latest_foo && latest_bar
+        puts "##### #{latest_foo} + #{latest_bar}"
+        foobar = self.assemble_foo_and_bar(latest_foo, latest_bar)
+        @channel.send(foobar)
+      else
+        @channel.send(nil)
+      end
+
     else
       @channel.send(nil)
     end
-
   end
 
-  def mine_bar() : Bar
+  def mine_bar : Bar
     puts "#{@id} - Mining bar"
     mining_duration = Random.new.rand(@mine_bar_min_time..@mine_bar_max_time)
     sleep mining_duration.seconds
@@ -79,7 +108,7 @@ class Robot
     Bar.new(bar_value)
   end
 
-  def mine_foo() : Foo
+  def mine_foo : Foo
     puts "#{@id} - Mining foo"
     sleep @mine_foo_time.seconds
     foo_value = UUID.random.to_s
@@ -88,12 +117,12 @@ class Robot
   end
 
   def assemble_foo_and_bar(foo : Foo, bar : Bar) : FooBar
-    { foo, bar }
+    FooBar.new(foo, bar)
   end
-
 end
 
-activity_channel = Channel(Nil|Foo|Bar|FooBar).new
+activity_channel = Channel(Nil | Foo | Bar | FooBar).new
+stock_channel = Channel(Array(Foo) | Array(Bar)).new
 
 start_time = Time.monotonic
 
@@ -105,7 +134,7 @@ foobars = [] of FooBar
 
 # Create the good number of robots
 robots_size.times do |i|
-  robot = Robot.new(i, activity_channel)
+  robot = Robot.new(i, activity_channel, stock_channel)
   robots << robot
 end
 
@@ -114,6 +143,8 @@ end
   # Spawn a task per robot
   robots.each do |robot|
     spawn robot.choose_activity
+    stock_channel.send(foos)
+    stock_channel.send(bars)
   end
 
   # Wating for the activities results
@@ -133,12 +164,11 @@ end
   end
 end
 
-
-puts "-> List of Foo"
+puts "-> List of Foo (#{foos.size})"
 puts foos
-puts "-> List of Bar"
+puts "-> List of Bar (#{bars.size})"
 puts bars
-puts "-> List of FooBar"
+puts "-> List of FooBar (#{foobars.size})"
 puts foobars
 
 end_time = Time.monotonic
